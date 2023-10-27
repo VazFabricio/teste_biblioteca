4cdb6a29297f89b2054d172125a6a06ff32ad3ca
Fabricio de Assis Vaz <fabricio.vaz@gmail.com>
2023-10-26 21:29:10 -0300

    mudanca nome

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='dsvallourec-lib',
-    packages=find_packages(include=['dsvallourec-lib']),
+    name='dsvalloureclib',
+    packages=find_packages(include=['dsvalloureclib']),
     version='0.1.0',
     install_requires=['scikit-learn'],
     description='My first Python library',
